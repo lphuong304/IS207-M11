75b9b216e1d9d6acab98edaec4477fc08bbd3e7e
LAN PHƯƠNG <19520227@gm.uit.edu.vn>
2021-09-20 22:58:39 +0700

    upload lab1 in class

---
+++ Labs/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+
+<html lang="vi">
+
+<head>
+
+	<meta charset="UTF-8">
+
+	<title>IS207.M11 - Nguyễn Ngọc Lan Phương - 19520227 - Bài tập thực hành Tuần 1</title>
+
+	<link rel="shortcut icon" href="https://en.uit.edu.vn/sites/vi/files/favicon.ico" type="image/vnd.microsoft.icon" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta content="IS207.M11 - Nguyễn Ngọc Lan Phương- 19520227 - Bài tập thực hành Tuần 1" name="description">
+
+	<meta content="Nguyen Ngoc Lan Phuong" name="author">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<style>
+
+		h1, h2, h3 {
+
+			text-align: center;
+
+		}
+
+	</style>
+
+</head>
+
+<body>
+
+	<div class="container">
+
+		<h1>IS207.M11 - Phát triển ứng dụng Web</h1>
+
+		<h2>Bài tập thực hành Tuần 1</h2>
+
+		<h3>Author: Nguyễn Ngọc Lan Phương- 19520227</h3>            
+
+		<table class="table table-hover">
+
+			<thead>
+
+			  <tr>
+
+				<th>STT</th>
+
+				<th>Tên bài thực hành</th>
+
+				<th>URL</th>
+
+			  </tr>
+
+			</thead>
+
+			<tbody>
+
+				<?php
+
+					$url_root = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+					$k = 1;
+
+					foreach (array_filter(glob('*'), 'is_dir') as $folder) {
+
+						echo '<tr><td>'.$k.'</td>';
+
+						echo '<td>BÃ i '.$k.'</td>';
+
+						echo '<td><a href="'.$url_root.$folder.'">'.$folder.'</a></td></tr>';
+
+						$k++;
+
+					}
+
+				?>
+
+			</tbody>
+
+		</table>
+
+	</div>
+
+</body>
+
+</html>
